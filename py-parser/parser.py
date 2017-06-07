333f0228980fefea2ae7edc48e0a3f8814820de2
vitornl <vitornaslourenco@gmail.com>
2017-06-07 15:36:36 -0300

    refatoracao do parser; fatos negativos; consultas

--- py-parser/parser.py
+++ py-parser/parser.py
@@ -6,35 +6,44 @@ from color_print import cprint
     
 def main():
     INPUT = sys.argv[1]
-    OUTPUT_POS = sys.argv[2]
-    OUTPUT_REL = sys.argv[3]
-    OUTPUT_NEG = sys.argv[4]
+    OUTPUT_FPOS = sys.argv[2]
+    OUTPUT_RPOS = sys.argv[3]
+    OUTPUT_FNEG = sys.argv[4]
+    OUTPUT_RNEG = sys.argv[5]
+    
     cprint("Reading " + INPUT + "...", 'b')
-    cprint("Creating " + OUTPUT_POS + "...", 'b')
-    kb_file_pos =  open(OUTPUT_POS, 'w')
-    cprint("Creating " + OUTPUT_NEG + "...", 'b')
-    kb_file_neg = open(OUTPUT_NEG, 'w')
-    cprint("Creating " + OUTPUT_REL + "...", 'b')
-    kb_file_rel =  open(OUTPUT_REL, 'w')
+    
+    cprint("Creating " + OUTPUT_FPOS + "...", 'b')
+    kb_file_fpos =  open(OUTPUT_FPOS, 'w')
+    cprint("Creating " + OUTPUT_RPOS + "...", 'b')
+    kb_file_rpos =  open(OUTPUT_RPOS, 'w')
+ 
+    cprint("Creating " + OUTPUT_FNEG + "...", 'b')
+    kb_file_fneg = open(OUTPUT_FNEG, 'w')
+    cprint("Creating " + OUTPUT_RNEG + "...", 'b')
+    kb_file_rneg =  open(OUTPUT_RNEG, 'w')
+    
     with open(INPUT, newline='') as csvfile:
         data = csv.DictReader(csvfile)      
         for row in data:
             if(row["Action"][0] == "n"): #neg relations
-                if(row["Relation"] == "generalizations"): continue
-                else:
-                    clause = ":- \+" + row["Relation"]
-                    if(clause == "haswikipediaurl"):
-                        clause = clause + "(" + row["Entity"] + ",'" + row["Value"] + "').\n"
-                    else:
-                        clause = clause + "(" + row["Entity"] + "," + row["Value"] + ").\n"
+                if(row["Relation"] == "generalizations"):
+                    clause = ":- \+" + row["Action"][1:] + "(" + row["Entity"] + ").\n"
                     cprint(clause, 'r')
-                    kb_file_neg.write(clause)
+                    kb_file_fneg.write(clause)
+                else:
+                    clause = "(" + row["Entity"] + ",'" + row["Value"] + "').\n" \
+                        if row["Relation"] == "haswikipediaurl" \
+                        else "(" + row["Entity"] + "," + row["Value"] + ").\n"
+                    clause = ":- \+" + row["Relation"] + clause
+                    cprint(clause, 'r')
+                    kb_file_rneg.write(clause)
 
             elif(row["Action"][0] == "y"):
                 if(row["Relation"] == "generalizations"):
                     clause = row["Action"][1:] + "(" + row["Entity"] + ").\n"
                     cprint(clause, 'y')
-                    kb_file_pos.write(clause)
+                    kb_file_fpos.write(clause)
                 else:
                     #facts
                     clause = row["Action"].split(",")[2]
@@ -45,25 +54,25 @@ def main():
                     else:
                         clause = clause + "(" + row["Value"] + ").\n"
                     cprint(clause, 'y')
-                    kb_file_pos.write(clause)
+                    kb_file_fpos.write(clause)
                     
                     clause = row["Action"].split(",")[1] + "(" + row["Entity"] + ").\n"
                     cprint(clause, 'y')
-                    kb_file_pos.write(clause)
+                    kb_file_fpos.write(clause)
                     
                     #relations
-                    clause = row["Relation"]
-                    if(clause == "haswikipediaurl"):
-                        clause = clause + "(" + row["Entity"] + ",'" + row["Value"] + "').\n"
-                    else:
-                        clause = clause + "(" + row["Entity"] + "," + row["Value"] + ").\n"
+                    clause = "(" + row["Entity"] + ",'" + row["Value"] + "').\n" \
+                        if row["Relation"] == "haswikipediaurl" \
+                        else "(" + row["Entity"] + "," + row["Value"] + ").\n"
+                    clause = row["Relation"] + clause
                     cprint(clause, 'y')
-                    kb_file_rel.write(clause)
+                    kb_file_rpos.write(clause)
             else: continue
         
-        kb_file_pos.close()
-        kb_file_rel.close()     
-        kb_file_neg.close()
+        kb_file_fpos.close()
+        kb_file_rpos.close()
+        kb_file_fneg.close()    
+        kb_file_rneg.close()
     
 if __name__ == "__main__":
      main()
