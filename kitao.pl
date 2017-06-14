2cb49d58bf319cd20c5a3e74a85f93a8dcdb8f59
simasgrilo <simasgrilo86@gmail.com>
2017-06-14 16:31:56 -0300

    algumas consultas
    porém a resposta delas estão bem pobrinhas.

---
+++ kitao.pl
@@ -0,0 +1,4 @@
+musicianplaysinstrument(X,guitar) ,musicartist(X), musicianinmusicartist(X,Y).%todos os artistas que tocam guitarra e tocam em um determinado gênero
+citycapitalofcountry(X,france), city(X),buildinglocatedincity(Y,X).%construções localizadas na cidade que é a capital da frança
+writer(X), bookwriter(Y,X), haswikipediaurl(Y,Z).%recupera os escritores que escreveram pelo menos um livro que está na wikipedia
+
