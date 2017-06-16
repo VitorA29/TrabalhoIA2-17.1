c4b577a6b917caedccdc9d852e1b5905eb8590f4
Max Fratane <mfratane@id.uff.br>
2017-06-15 21:56:12 -0300

    .

---
+++ pl/crack.pl
@@ -0,0 +1,9 @@
+rel(rio_de_janeiro, possui, assalto).
+rel(sao_gonçalo, possui, assalto).
+rel(niteroi, possui, qualidade).
+rel(niteroi, possui, UFF).
+rel(UFF, oferece, computacao).
+rel(UFF, possui, comunista).
+rel(comunista, gostade, daropopo).
+rel(pedra, ficaem, niteroi).
+rel(pedra, faz, comidamerda).
