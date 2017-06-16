470b2215530dfb5bc75314a5f00206d3da2f7f25
vitornl <vitornaslourenco@gmail.com>
2017-06-15 23:33:35 -0300

    normalizacao do git

--- Implementação2/bk.pl
+++ Implementação2/bk.pl
@@ -8953,7 +8953,3 @@ rel(american_spectator,generalizations,publication).
 rel(american_spectator,generalizations,website).
 rel(mount_everest,generalizations,mountain).
 rel(acid_oxidation,generalizations,chemical).
-
-go(X,Y):-(\+(X=Y)),write(X),write(":e:\n"),rel(X,Z,_),write(Z),write(":r:\n"),rel(Y,Z,_),write(Y),write(":e:\n"),!.
-go(X,Y):-write(X),write(":e:\n"),rel(X,Z,V),write(Z),write(":r:\n"),write(V),write(":v:\n"),rel(Y,W,V),write(W),write(":r:\n"),write(Y),write(":e:\n"),\+(X=Y),!.
-go(X,Y):-write(X),write(":e:\n"),rel(X,Z,K),write(Z),write(":r:\n"),write(K),write(":v:\n"),rel(W,M,K),write(M),write(":r:\n"),write(W),write(":e:\n"),go(W,Y).
